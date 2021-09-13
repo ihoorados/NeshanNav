7623ad5eb6e00dc58f7cd75995acd37c0637699b
ihoorados <hooradramezani@gmail.com>
2021-09-13 16:35:18 +1000

    +

---
+++ NeshanNav/CodeBase/Report/ReportCollectionView.swift
@@ -0,0 +1,52 @@
+//
+//  ReportCollectionView.swift
+//  NeshanNav
+//
+//  Created by Hoorad Ramezani on 13/9/21.
+//
+
+import Foundation
+import UIKit
+
+class ReportCollectionView: UIViewController {
+    
+    /* ////////////////////////////////////////////////////////////////////// */
+    // MARK: Dependency Injection
+    /* ////////////////////////////////////////////////////////////////////// */
+    
+    let viewModel: SendReportViewModel = SendReportViewModelImpl()
+
+    /* ////////////////////////////////////////////////////////////////////// */
+    // MARK: Lifecycle
+    /* ////////////////////////////////////////////////////////////////////// */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    /* ////////////////////////////////////////////////////////////////////// */
+    // MARK: UI Properties
+    /* ////////////////////////////////////////////////////////////////////// */
+
+    
+    let cellIdentifire = "cellIdentifire"
+    var reportBoxCollectionView : UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 8.0
+        layout.minimumInteritemSpacing = 8.0
+        let CollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        CollectionView.translatesAutoresizingMaskIntoConstraints = false
+        CollectionView.showsVerticalScrollIndicator = false
+        CollectionView.showsHorizontalScrollIndicator = false
+        CollectionView.backgroundColor = .clear
+        return CollectionView
+    }()
+    
+    
+    
+
+}
